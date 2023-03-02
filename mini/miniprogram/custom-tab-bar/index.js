73defb47b99f4ce262b9c9c149b1ba8d74b9a985
Cui.Jiaqing <291527070@qq.com>
2023-03-02 16:26:42 +0800

    小程序初始化

---
+++ mini/miniprogram/custom-tab-bar/index.js
@@ -0,0 +1,43 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
+    list: [{
+      pagePath: "/pages/index/index",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "菜单"
+    }, {
+      pagePath: "/pages/my/my",
+      iconPath: "/image/icon_API.png",
+      selectedIconPath: "/image/icon_API_HL.png",
+      text: "我的"
+    }]
+  },
+  lifetimes:{
+    created:function(){
+      console.log(   getCurrentPages())
+
+
+    }
+  }
+  ,
+  pageLifetimes: {
+    show: function () {
+      console.log("页面熏染")
+    }
+  }
+  ,
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({
+        url: url,
+      })
+    }
+  }
+})
